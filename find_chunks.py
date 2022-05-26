275a993d1ab1ebe3c363103325cfd3e97f2be708
NZambare <57184218+NZambare@users.noreply.github.com>
2022-05-26 11:00:40 +0530

    Added chunk identifier

---
+++ find_chunks.py
@@ -0,0 +1,40 @@
+import main
+
+
+def find_chunks(board, chunk_num):
+    queue = []
+    checked_squares = []
+    chunks = []
+    cur_chunk = []
+    for y, row in enumerate(board):
+        for x, item in enumerate(row):
+            if (x, y) in checked_squares:
+                continue
+            if item != 0:
+                continue
+            queue.insert(0, (x, y))
+            checked_squares.append((x, y))
+            while len(queue) > 0:
+                cur_x = queue[-1][0]
+                cur_y = queue[-1][1]
+                if board[cur_y][cur_x] == 0:
+                    cur_chunk.append((cur_x, cur_y))
+                    for offset in main.OFFSETS:
+                        new_x = cur_x + offset[0]
+                        new_y = cur_y + offset[1]
+                        if 0 <= new_x < len(board[0]) and 0 <= new_y < len(board):
+                            if (new_x, new_y) not in checked_squares and (new_x, new_y) not in queue:
+                                queue.insert(0, (new_x, new_y))
+                checked_squares.append((cur_x, cur_y))
+                del queue[-1]
+            chunks.append(cur_chunk[:])
+            cur_chunk.clear()
+    return chunks
+
+
+def raw_chunks_to_board(raw_chunks, w, h):
+    board = [[0 for _ in range(w)] for _ in range(h)]
+    for i, chunk in enumerate(raw_chunks):
+        for coord in chunk:
+            board[coord[1]][coord[0]] = i+1
+    return board
--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 import main
+import find_chunks as fc
 from pygame_utils import *
 
 
@@ -12,28 +13,37 @@ screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pg.display.set_caption("MineSweeper")
 
 tile_layer = [[((i+j) % 2) + 1 for i in range(main.WIDTH)] for j in range(main.HEIGHT)]
+debug_map = fc.raw_chunks_to_board(fc.find_chunks(main.board, 0), main.WIDTH, main.HEIGHT)
 
 
 tile_images = [None]
 tile_images.extend(load_images("img/tiles", ("png", "jpg")))
 
-
 item_images = scale_list(load_images("img/items", ("png", "jpg")), (32, 32))
+
+debug_tile_images = [None]
+debug_tile_images.extend(load_images("img/debug_tiles"))
 
 
 running = True
+debug = True
 while running:
 
     screen.fill((100, 100, 100))
 
     draw_list(screen, main.board, item_images, (0, 0), (32, 32))
-    draw_list(screen, tile_layer, tile_images, (0, 0), (32, 32), [0])
+    if debug:
+        draw_list(screen, debug_map, debug_tile_images, (0, 0), (32, 32), [0])
+    # draw_list(screen, tile_layer, tile_images, (0, 0), (32, 32), [0])
 
     pg.display.flip()
 
     for e in pg.event.get():
         if e.type == pg.QUIT:
             running = False
+        if e.type == pg.KEYDOWN:
+            if e.key == pg.K_TAB:
+                debug = not debug
 
 
 pg.quit()
